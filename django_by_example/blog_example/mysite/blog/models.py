1bc50f2c0c5df2dddf307b96b4bc07125efec13f
alex chan <chan.k.leong@gmail.com>
2017-06-16 22:43:09 +0800

    Added PublishedManager

--- django_by_example/blog_example/mysite/blog/models.py
+++ django_by_example/blog_example/mysite/blog/models.py
@@ -6,6 +6,14 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 
+
+
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,
+                     self).get_queryset().filter(status='published')
+
+
 class Post(models.Model):
     STATUS_CHOICES = (
         ('draft', 'Draft'),
@@ -23,9 +31,15 @@ class Post(models.Model):
     status = models.CharField(max_length=10, 
                               choices=STATUS_CHOICES,
                               default='draft')
+    
+    objects = models.Manager() # default manager
+    published = PublishedManager() # custom manager
+
+    
 
     class Meta:
         ordering = ('-publish',)
 
     def __str__(self):
         return self.title
+
